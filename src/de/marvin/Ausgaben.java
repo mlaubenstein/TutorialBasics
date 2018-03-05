d1162c3c1b17124daa753b05b11f945006c270d4
Marvin Laubenstein <37064890+mlaubenstein@users.noreply.github.com>
2018-03-05 14:47:17 +0100

    ErrorOutput/Scanner-Object
    Comments included.

--- src/de/marvin/Ausgaben.java
+++ src/de/marvin/Ausgaben.java
@@ -2,9 +2,20 @@ package de.marvin;
 
 
 
+
 public class Ausgaben {
-    public static void HelloWorldOut(String str){
+
+    public  void HelloWorldOut(String str){
         System.out.println ( str );
     }
 
+
+    public void ErrorOut(String str) {
+        System.err.println ( "ERROR, String is NULL!" );
+
+    }
+
+    public void ScannerOut(String inData) {
+        System.out.println ("Output: "+ inData );
+    }
 }
--- src/de/marvin/Main.java
+++ src/de/marvin/Main.java
@@ -1,17 +1,32 @@
 package de.marvin;
+import  java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         String str = new String ( "Hello World !" );
-        String sub = str.substring ( 0, 13);
-        Ausgaben ausgabenObject = new Ausgaben();
+        String sub = str.substring ( 0, 13 );
+        Scanner scan = new Scanner ( System.in );
+        String inData;
 
+        Ausgaben ausgabenObject = new Ausgaben ();
 
-        ausgabenObject.HelloWorldOut (sub);
+        //Simple HelloWorld
+        ausgabenObject.HelloWorldOut ( sub );
+
+        //Output of an Error
+        str = null;
+        ausgabenObject.ErrorOut ( str );
+
+        //Input and Output of a Scanner-Object
+        System.out.println ( "Input:" );
+        inData = scan.nextLine ();
+        ausgabenObject.ScannerOut (inData);
 
 
 
     }
 }
+
+
